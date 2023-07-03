842e772773813874156c79e939a6e74ac7acdd2f
Seba1996c <sebacrash.f2@gmail.com>
2023-07-02 23:46:08 -0400

    primera fase

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Movie.create(name: "Movie 1", synopsis: "Synopsis 1", director: "Director 1")
+Movie.create(name: "Movie 2", synopsis: "Synopsis 2", director: "Director 2")
+# Agrega más datos para las películas
+
+Serie.create(name: "Serie 1", synopsis: "Synopsis 1", director: "Director 1")
+Serie.create(name: "Serie 2", synopsis: "Synopsis 2", director: "Director 2")
+# Agrega más datos para las series
+
+DocumentaryFilm.create(name: "Documentary 1", synopsis: "Synopsis 1", director: "Director 1")
+DocumentaryFilm.create(name: "Documentary 2", synopsis: "Synopsis 2", director: "Director 2")
+# Agrega más datos para los documentales
